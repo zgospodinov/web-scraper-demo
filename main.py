85e74eca381f5c5f9ec5276904331e766aa07e06
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2025-06-21 19:41:28 +0300

    Tour event: Send email notification integration

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
 import selectorlib
 import os
+from dotenv import load_dotenv
+import smtplib
+from email.message import EmailMessage
+import ssl
 
+
+load_dotenv()
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 STORAGE_FILE = os.path.join(SCRIPT_DIR, 'data.txt')
@@ -28,8 +34,29 @@ def extract_data(content):
     return value
     
 
-def send_email():
-    print("Sending email... (This function is not implemented yet)")
+def send_email(tour_info):
+    print("Sending email...")
+
+    host = "smtp.gmail.com"
+    port = 465  # For SSL
+
+    user_name = os.getenv("EMAIL_USERNAME")
+    password = os.getenv("APP_EMAIL_KEY")
+
+    if not user_name or not password:
+        raise ValueError("Email credentials not found in .env file. Please check your .env file configuration.")
+
+    context = ssl.create_default_context()
+
+    msg = EmailMessage()
+    msg['From'] = user_name
+    msg['To'] = user_name
+    msg['Subject'] = 'New Tour Available!'
+    msg.set_content(tour_info)
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_name, password)
+        server.send_message(msg)
     
 
 def store(data, filename=STORAGE_FILE):
@@ -52,7 +79,7 @@ if __name__ == "__main__":
         stored_data = read_stored()
         if extracted != 'No upcoming tours' and extracted not in stored_data:
             store(extracted)
-            send_email()
+            send_email(extracted)
         
         print(extracted)
     except Exception as e:
